8da6894d4caacce7144d3f03836763e800a37ba0
Karthikeyan SR <srkarthik27@gmail.com>
2025-11-21 21:05:44 +0530

    Changes line 4 in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-FASTAPI_URL = "http:127.0.0.1:8000"
+FASTAPI_URL = "http//127.0.0.1:8000"
 
 st.set_page_config(page_title="PDF Query Bot", layout="wide")
 
